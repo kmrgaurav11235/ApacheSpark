956f80ea6da216ff95893cea620716eef5fbe00e
Kumar Gaurav <kgaurav3@vmware.com>
2021-02-23 12:08:02 +0530

    Small test

--- src/main/java/in/gaurav/Main.java
+++ src/main/java/in/gaurav/Main.java
@@ -1,7 +1,17 @@
 package in.gaurav;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        List<Double> inputData = new ArrayList<>();
+        inputData.add(3.141);
+        inputData.add(12.678);
+        inputData.add(31.789);
+        inputData.add(45.891);
+        inputData.add(12.441);
+
+        System.out.println(inputData);
     }
 }
