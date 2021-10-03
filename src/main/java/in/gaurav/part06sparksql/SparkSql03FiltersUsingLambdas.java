5138f44c64e6c4389f6d810883a032dc3c784dc1
Kumar Gaurav <kmrgaurav11235@gmail.com>
2021-10-03 16:47:36 +0530

    Filters using Lambdas

---
+++ src/main/java/in/gaurav/part06sparksql/SparkSql03FiltersUsingLambdas.java
@@ -0,0 +1,32 @@
+package in.gaurav.part06sparksql;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.spark.api.java.function.FilterFunction;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+
+public class SparkSql03FiltersUsingLambdas {
+    public static void main(String[] args) {
+        Logger.getLogger("org.apache").setLevel(Level.WARN);
+
+        SparkSession spark = SparkSession.builder()
+                .appName("testingSql").master("local[*]")
+                .getOrCreate();
+
+        final Dataset<Row> dataset = spark.read()
+                .option("header", true)
+                .csv("src/main/resources/exams/students.csv");
+
+        // Task: Filter the data -> only include the subject "Modern Art" and after year 2006
+
+        // Using Lambdas: Each element is of type Row
+        final Dataset<Row> modernArtResults = dataset.filter((Row row) ->
+                row.getAs("subject").equals("Modern Art")
+                        && Integer.parseInt(row.getAs("year")) >= 2007);
+        modernArtResults.show();
+
+        spark.close();
+    }
+}
