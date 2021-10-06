f650daecb9e4f6db2efbabb6e224c64bd9e22e01
Kumar Gaurav <kmrgaurav11235@gmail.com>
2021-10-06 08:09:30 +0530

    Groupings and Aggregations

---
+++ src/main/java/in/gaurav/part07sparksql2/SparkSql04GroupingsAndAggregations.java
@@ -0,0 +1,52 @@
+package in.gaurav.part07sparksql2;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.RowFactory;
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.types.DataTypes;
+import org.apache.spark.sql.types.Metadata;
+import org.apache.spark.sql.types.StructField;
+import org.apache.spark.sql.types.StructType;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SparkSql04GroupingsAndAggregations {
+    public static void main(String[] args) {
+        Logger.getLogger("org.apache").setLevel(Level.WARN);
+
+        SparkSession spark = SparkSession.builder()
+                .appName("testingSql").master("local[*]")
+                .getOrCreate();
+
+        List<Row> inMemory = new ArrayList<>();
+        inMemory.add(RowFactory.create("WARN", "2016-12-31 04:19:32"));
+        inMemory.add(RowFactory.create("FATAL", "2016-12-31 03:22:34"));
+        inMemory.add(RowFactory.create("WARN", "2016-12-31 03:21:21"));
+        inMemory.add(RowFactory.create("INFO", "2015-4-21 14:32:21"));
+        inMemory.add(RowFactory.create("FATAL","2015-4-21 19:23:20"));
+
+        // Each StructField inside StructType represents a column in out data structure
+        StructField[] fields = {
+                new StructField("level", DataTypes.StringType, false, Metadata.empty()),
+                new StructField("datetime", DataTypes.StringType, false, Metadata.empty())
+        };
+
+        StructType schema = new StructType(fields);
+
+        // DataFrame is just a Dataset of Row, i.e. Dataset<Row>
+        final Dataset<Row> dataset = spark.createDataFrame(inMemory, schema);
+
+        dataset.createOrReplaceTempView("logging_view");
+
+        // We must do any type aggregation on the other columns
+        // Docs: https://spark.apache.org/docs/latest/api/sql/index.html
+        final Dataset<Row> results = spark.sql("select level, collect_list(datetime) from logging_view group by level order by level");
+        results.show();
+
+        spark.close();
+    }
+}
