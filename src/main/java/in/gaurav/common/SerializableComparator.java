ff7e087f752dfb8ae623b2d199eabd366ec33385
Kumar Gaurav <kmrgaurav11235@gmail.com>
2021-10-20 21:23:50 +0530

    Log count RDD version

---
+++ src/main/java/in/gaurav/common/SerializableComparator.java
@@ -0,0 +1,10 @@
+package in.gaurav.common;
+
+import java.io.Serializable;
+import java.util.Comparator;
+
+public interface SerializableComparator<T> extends Comparator<T>, Serializable {
+    static <T> SerializableComparator<T> serialize(SerializableComparator<T> comparator) {
+        return comparator;
+    }
+}
